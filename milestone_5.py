367b72a1ec3d2e12e1dcbf023acbc7a0afc5cfcd
Cdawgbrodizzle <c-brady-work@live.co.uk>
2023-12-28 16:37:52 +0000

    milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -2,11 +2,12 @@ import random
 word_list = ["apple", "orange", "mango", "pear", "melon"]
 
 hangman_word = random.choice(word_list)
-list_of_guesses = []
 correct_letters = [str(hangman_word)]
 print(hangman_word)
+list_of_guesses = []
 
 class Hangman:
+
     def __init__(self, num_lives, word_list, num_letters):
         self.hangman_word = random.choice(word_list)
         self.word_guessed = "_" * len(hangman_word)
@@ -14,7 +15,7 @@ class Hangman:
         self.word_list = ["apple", "orange", "mango", "pear", "melon"]
         self.list_of_guesses = []
 
-        def ask_for_input(self):
+        def ask_for_input():
                 while True:
                     guess = input("Guess a letter: ").lower()
                     if len(guess) > 1 or not guess.isalpha():
@@ -25,9 +26,9 @@ class Hangman:
                         continue
                     else:
                         break
-                self.verify_guess()
                 return guess
-        guessed = ask_for_input(self)
+        guessed = ask_for_input()
+        
         def check_guess(guessed, num_letters, num_lives):
             word_guessed = "_" * len(hangman_word)
             if guessed.lower() in hangman_word:
@@ -37,7 +38,6 @@ class Hangman:
                         word_guessed = word_guessed[:i] + hangman_word[i] + word_guessed[i+1:]
                         num_letters = num_letters - 1
                         return word_guessed, num_letters
-
             else:
                 num_lives = num_lives - 1
                 print("Sorry, {guessing} is hard isn't it? Try again.")
@@ -46,20 +46,22 @@ class Hangman:
         verify_guess = check_guess(guessed, num_letters, num_lives)
         print(verify_guess)
 
-        def play_game(word_list):
-            num_lives = 5
-            while True:
-                if num_lives == 0:
-                    print("You Lost")
-                    break
-                elif num_lives >= 0 and num_letters >= 0:
-                    ask_for_input()
-                    continue
-                else: 
-                    print("congratulations, you won the game!")
 
-        play_game(word_list=["apple", "orange", "mango", "pear", "melon"])
-            
-game = Hangman(num_lives=5, word_list=["apple", "orange", "mango", "pear", "melon"], num_letters = len(hangman_word))
+#Hangman(num_lives=5, word_list=["apple", "orange", "mango", "pear", "melon"], num_letters = len(hangman_word))
 
-print(game)
+def play_game(word_list, num_letters):
+    num_lives = 5
+    game = Hangman(word_list=["apple", "orange", "mango", "pear", "melon"], num_lives = 5, num_letters = len(hangman_word))
+    while True:
+        if num_lives == 0:
+            print("You Lost")
+            break
+        elif num_lives >= 0 and game.num_letters >= 0:
+            game.guessed()
+            break
+        else: 
+            print("congratulations, you won the game!")
+            break  
+#play_game(word_list, num_letters = len(hangman_word))
+if __name__ == '__main__':
+    play_game(word_list=["apple", "orange", "mango", "pear", "melon"], num_letters=len(hangman_word))
